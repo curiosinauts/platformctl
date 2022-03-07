4e9d274415686e16bb48babf7d98aa8f3092ac49
7onetella <7onetella@gmail.com>
2022-03-06 23:07:22 -0500

    updating sites

--- containers/nginx/build.sh
+++ containers/nginx/build.sh
@@ -4,16 +4,12 @@ set -x
 
 service=sites
 
-# group=public
-
 cp ~/.ssh/authorized_keys .ssh/
 
-# version=$(vag docker version patch ${service}-${group})
-
-version=$(platformctl next tag 7onetella/sites)
+version=$(platformctl next tag curiosinauts/sites)
+# version=0.1.0
 
 docker build -t docker-registry.curiosityworks.org/curiosinauts/${service}:"${version}" .
 
 docker push docker-registry.curiosityworks.org/curiosinauts/${service}:"${version}"
 
-# nomad job stop -purge ${service}
