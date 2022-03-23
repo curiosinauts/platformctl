81305951569130734dc2664785681bf4b11694b5
7onetella <7onetella@gmail.com>
2022-03-22 23:07:04 -0400

    refactoring before docker build

--- cmd/before_docker_build.go
+++ cmd/before_docker_build.go
@@ -25,70 +25,36 @@ var beforeDockerBuildCmd = &cobra.Command{
 
 		eh := ErrorHandler{"before docker-build"}
 
-		if !io.DoesPathExists("./.ssh") {
-			err := os.Mkdir("./.ssh", 0755)
-			eh.HandleError("creating .ssh folder", err)
-		}
-
 		user := database.User{}
 		eh.HandleError("finding user by username", dbs.FindBy(&user, "username=$1", username))
 
-		io.WriteStringTofile(user.PrivateKey, "./.ssh/id_rsa")
+		// code server required authentication file
+		createCoderConfigYamlFile(user.Password)
 
-		// codeserver .config.yml
-		io.WriteTemplate(`bind-addr: 0.0.0.0:9991
-auth: password
-password: {{.}}
-cert: false `, user.Password, "./config.yml")
+		// user's private terminal app
+		createGottySSH(user, eh)
 
-		// .gitconfig
-		io.WriteTemplate(`[credential]
-    helper = store
-[user]
-	name = {{.Username}}
-	email = {{.Email }}`, user, "./.gitconfig")
+		// id_rsa key for git authentication
+		createSSHFolder(eh)
+		createIDRSAKey(user.PrivateKey)
 
+		// .gitconfig for git operation
+		createGITConfig(user)
+
+		// git clone of user projects
 		repositories := []string{user.GitRepoURI}
-		// repositories.txt
-		io.WriteTemplate(`{{range $val := .}}
-{{$val}}
-{{end}}`, repositories, "./repositories.txt")
+		createRepositoriesTXT(repositories)
 
+		// self service runtime installs config file
 		runtimeInstalls := []database.RuntimeInstall{}
 		dbs.FindAllRuntimeInstallsForUser(&runtimeInstalls, username)
-		io.WriteTemplate(`#!/bin/zsh -e
-    
-set -x
-{{range $v := .}}
-{{$v.ScriptBody}}
-{{end}}`, runtimeInstalls, "./runtime_install.sh")
-
-		err := os.Chmod("./runtime_install.sh", 0755)
-		eh.HandleError("writing runtime install", err)
-
-		io.WriteTemplate(`#!/bin/sh
-
-export TERM=xterm
-
-/home/coder/go/bin/gotty --ws-origin "vscode-{{.Username}}.curiosityworks.org" -p 2222 -c "{{.Username}}:{{.Password}}" -w /usr/bin/zsh >>/dev/null 2>&1 
-`, user, "./gotty.sh")
-		err = os.Chmod("./gotty.sh", 0755)
-		eh.HandleError("writing .gotty.sh", err)
+		createRuntimeInstallSSHFile(runtimeInstalls, eh)
 
 		user.DockerTag = dockertag
 		io.WriteTemplate(deployServiceIngressTemplate, user, "./vscode-"+user.Username+".yml")
 
-		user.PostgresUsername = strings.Replace(user.Username, "-", "", -1)
-		user.PGHost = viper.Get("shared_database_host").(string)
-		user.PGDBName = viper.Get("shared_database_name").(string)
-		io.WriteTemplate(`
-export PGUSER={{.PostgresUsername}}
-export PGPASSWORD={{.Password}}
-export PGHOST={{.PGHost}}
-export PGDATABASE={{.PGDBName}}
-`, user, "./.exports")
-		err = os.Chmod("./.exports", 0755)
-		eh.HandleError("writing .exports", err)
+		// environment variables for things like postgresl username and password
+		createDotExportsFile(user, eh)
 
 		msg.Success("before docker-build")
 	},
@@ -154,3 +120,72 @@ spec:
             port: 
               number: 80
 `
+
+func createSSHFolder(eh ErrorHandler) {
+	if !io.DoesPathExists("./.ssh") {
+		err := os.Mkdir("./.ssh", 0755)
+		eh.HandleError("creating .ssh folder", err)
+	}
+}
+
+func createIDRSAKey(privateKey string) {
+	io.WriteStringTofile(privateKey, "./.ssh/id_rsa")
+}
+
+func createCoderConfigYamlFile(password string) {
+	io.WriteTemplate(`bind-addr: 0.0.0.0:9991
+auth: password
+password: {{.}}
+cert: false `, password, "./config.yml")
+}
+
+func createGITConfig(user database.User) {
+	io.WriteTemplate(`[credential]
+    helper = store
+[user]
+	name = {{.Username}}
+	email = {{.Email }}`, user, "./.gitconfig")
+}
+
+func createRepositoriesTXT(repositories []string) {
+	io.WriteTemplate(`{{range $val := .}}
+{{$val}}
+{{end}}`, repositories, "./repositories.txt")
+}
+
+func createRuntimeInstallSSHFile(runtimeInstalls []database.RuntimeInstall, eh ErrorHandler) {
+	io.WriteTemplate(`#!/bin/zsh -e
+    
+set -x
+{{range $v := .}}
+{{$v.ScriptBody}}
+{{end}}`, runtimeInstalls, "./runtime_install.sh")
+	err := os.Chmod("./runtime_install.sh", 0755)
+	eh.HandleError("writing runtime install", err)
+
+}
+
+func createGottySSH(user database.User, eh ErrorHandler) {
+	io.WriteTemplate(`#!/bin/sh
+
+	export TERM=xterm
+	
+	/home/coder/go/bin/gotty --ws-origin "vscode-{{.Username}}.curiosityworks.org" -p 2222 -c "{{.Username}}:{{.Password}}" -w /usr/bin/zsh >>/dev/null 2>&1 
+	`, user, "./gotty.sh")
+	err := os.Chmod("./gotty.sh", 0755)
+	eh.HandleError("writing .gotty.sh", err)
+}
+
+func createDotExportsFile(user database.User, eh ErrorHandler) {
+	user.PostgresUsername = strings.Replace(user.Username, "-", "", -1)
+	user.PGHost = viper.Get("shared_database_host").(string)
+	user.PGDBName = viper.Get("shared_database_name").(string)
+	io.WriteTemplate(`
+export PGUSER={{.PostgresUsername}}
+export PGPASSWORD={{.Password}}
+export PGHOST={{.PGHost}}
+export PGDATABASE={{.PGDBName}}
+`, user, "./.exports")
+	err := os.Chmod("./.exports", 0755)
+	eh.HandleError("writing .exports", err)
+}
